ae1c027072770cbf012cc730b4289b9b1d19e0e8
Ali Najafizadeh <a.najafizadeh@gmail.com>
2022-08-28 13:06:36 -0400

    Add checkbox feature

--- keycodes_other.go
+++ keycodes_other.go
@@ -1,3 +1,4 @@
+//go:build !windows
 // +build !windows
 
 package promptui
@@ -5,6 +6,9 @@ package promptui
 import "github.com/chzyer/readline"
 
 var (
+	// KeySpace is the default key to chose options for checkbox
+	KeySpace rune = 32
+
 	// KeyBackspace is the default key for deleting input text.
 	KeyBackspace rune = readline.CharBackspace
 )
